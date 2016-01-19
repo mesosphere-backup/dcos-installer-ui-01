30f31f46c59779824699b5c43698aae4c05fa8d8
Kenny Tran <kenny8tran@gmail.com>
2016-01-19 15:09:46 -0800

    Update ProcessStageUtil to most up to date knowledge of API

--- src/js/utils/ProcessStageUtil.js
+++ src/js/utils/ProcessStageUtil.js
@@ -1,15 +1,15 @@
-function processHostState(hostStatus, host, role, state) {
+function processHostState(hostState, host, role, state) {
   let stateType = state[`${role}s`];
+  let hostStatus = hostState.host_status;
+  stateType.totalStarted += 1;
 
-  stateType.total += 1;
-
-  if (hostStatus.state === 'running') {
+  if (hostStatus === 'running') {
     stateType.completed = false;
   }
 
-  if (hostStatus.state === 'error') {
+  if (hostStatus === 'error') {
     stateType.errors += 1;
-    state.errorDetails.push({host, errors: hostStatus.stderr});
+    state.errorDetails.push({host, errors: hostState.stderr});
   }
 }
 
@@ -17,21 +17,23 @@ const ProcessStageUtil = {
   processState(response) {
     let state = {
       slaves: {
+        completed: true,
         errors: 0,
-        total: 0,
-        completed: true
+        totalSlaves: response.total_slaves,
+        totalStarted: 0
       },
       errorDetails: [],
       masters: {
+        completed: true,
         errors: 0,
-        total: 0,
-        completed: true
+        totalMasters: response.total_masters,
+        totalStarted: 0
       }
     };
 
-    Object.keys(response).forEach(function (host) {
+    Object.keys(response.hosts).forEach(function (host) {
       let hostStatus = response[host];
-      processHostState(hostStatus, host, hostStatus.role, state);
+      processHostState(hostStatus, host, hostStatus.tags.role, state);
     });
 
     return state;
