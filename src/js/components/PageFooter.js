5c2753a70565965fd03e476aadb0adaa6d4ea0aa
John Furrow <johnf@mesosphere.io>
2016-01-26 13:59:30 -0800

    Add begin page links

--- src/js/components/PageFooter.js
+++ src/js/components/PageFooter.js
@@ -13,19 +13,19 @@ class PageFooter extends React.Component {
       <div className={classes}>
         <ul className="page-footer-links list-unstyled">
           <li>
-            <a href="#">System Requirements</a>
+            <a href="https://docs.mesosphere.com/getting-started/installing/system-requirements/" target="_blank">System Requirements</a>
           </li>
           <li>
-            <a href="#">Docs & Tutorials</a>
+            <a href="http://docs.mesosphere.com/" target="_blank">Docs & Tutorials</a>
           </li>
           <li>
-            <a href="#">Support</a>
+            <a href="https://docs.mesosphere.com/support/" target="_blank">Support</a>
           </li>
         </ul>
         <p className="page-footer-legal">
           &copy; {this.getYear()} Mesosphere, Inc. All Rights Reserved.
-          <a href="#">Terms Of Service</a>
-          <a href="#">Privacy Policy</a>
+          <a href="https://mesosphere.com/terms/" target="_blank">Terms Of Service</a>
+          <a href="https://mesosphere.com/privacy/" target="_blank">Privacy Policy</a>
         </p>
       </div>
     );
