2896adfbfb6f4f810c7e079c3b68e7ba7412d13d
John Furrow <johnf@mesosphere.io>
2016-05-27 14:13:20 -0700

    Fix IconWarning

--- src/js/components/icons/IconWarning.js
+++ src/js/components/icons/IconWarning.js
@@ -1,3 +1,4 @@
+import classnames from 'classnames';
 import React from 'react';
 
 class IconWarning extends React.Component {
@@ -9,8 +10,7 @@ class IconWarning extends React.Component {
         className={classes}
         height="64px"
         viewBox="0 0 64 64"
-        width="64px"
-        {...this.props}>
+        width="64px">
         <rect x="31" y="43" width="2" height="4"/>
         <rect x="31" y="17" width="2" height="22"/>
         <path d="M32,63A31,31,0,1,1,63,32,31,31,0,0,1,32,63ZM32,3A29,29,0,1,0,61,32,29,29,0,0,0,32,3Z"/>
