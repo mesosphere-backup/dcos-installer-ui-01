650917324b583ba770f95ef97b4d8a087989963c
John Furrow <johnf@mesosphere.io>
2016-06-02 13:58:19 -0700

    Combine constants

--- src/js/constants/NodeStatusDisplayText.js
+++
@@ -1,9 +0,0 @@
-import NodeStatuses from './NodeStatuses';
-
-module.exports = {
-  [NodeStatuses.FAILED]: 'Failed',
-  [NodeStatuses.RUNNING]: 'Running',
-  [NodeStatuses.SUCCESS]: 'Success',
-  [NodeStatuses.TERMINATED]: 'Terminated',
-  [NodeStatuses.UNSTARTED]: 'Pending'
-};
--- src/js/constants/NodeStatuses.js
+++ src/js/constants/NodeStatuses.js
@@ -1,7 +1,22 @@
 module.exports = {
-  FAILED: 'failed',
-  RUNNING: 'running',
-  SUCCESS: 'success',
-  TERMINATED: 'terminated',
-  UNSTARTED: 'unstarted'
+  FAILED: {
+    apiString: 'failed',
+    displayText: 'Failed'
+  },
+  RUNNING: {
+    apiString: 'running',
+    displayText: 'Running'
+  },
+  SUCCESS: {
+    apiString: 'success',
+    displayText: 'Success'
+  },
+  TERMINATED: {
+    apiString: 'terminated',
+    displayText: 'Terminated'
+  },
+  UNSTARTED: {
+    apiString: 'unstarted',
+    displayText: 'Pending'
+  }
 };
